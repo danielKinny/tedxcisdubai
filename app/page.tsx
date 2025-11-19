ebe5f324cf4d6475e733b815a0ca3a3e56fe40f6
daniel kinny <danielkinny0214@gmail.com>
2025-11-19 20:16:09 +0400

    inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,54 @@
 import Image from "next/image";
+import React from "react";
+const NAV_LINKS = ["Home", "About", "Events", "News", "Contacts"];
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+  return(
+    <div className="bg-white min-h-screen helvetica">
+      <header className="bg-white py-6">
+        <div className="grid grid-cols-3 items-center">
+          {/* left: brand */}
+          <div className="flex items-center gap-3">
+            <a href="#" className="inline-flex items-baseline">
+              <Image src="/logo-black.png" width={180} height={180} alt="logo"/>
+            </a>
+          </div>
+
+          {/* center: nav links */}
+          <nav className="flex justify-center w-full">
+            <ul className="flex gap-8 uppercase text-sm tracking-wider items-center">
+              {NAV_LINKS.map((label) => (
+                <li key={label}>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition-all hover:text-red-600"
+                  >
+                    <p className="hover:scale-105 transition-transform">
+                      {label}
+                      </p>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+              
+          {/* right: tickets button */}
+          <div className="flex flex-row justify-end">
+            {/* tickets button */}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              href="#"
+              className="ml-2 mr-8 inline-block bg-red-600 hover:bg-white hover:text-red-600 text-white text-sm font-semibold px-4 py-2 rounded-full shadow"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              Registration
+            </a>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+      </header>
+
+      <main className="max-w-7xl mx-auto helvetica px-6">
+        <video autoPlay loop className="w-full h-auto rounded-2xl">
+          <source src="/looky.mp4" type="video/mp4" />
+        </video>
       </main>
     </div>
   );
