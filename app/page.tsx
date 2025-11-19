cfc621090063eb84356c8d5db2bf0bad231d779e
daniel kinny <danielkinny0214@gmail.com>
2025-11-19 20:51:32 +0400

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,56 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 const NAV_LINKS = ["Home", "About", "Events", "News", "Contacts"];
+import {
+  ArrowTopRightOnSquareIcon
+} from "@heroicons/react/24/outline";
 
 export default function Home() {
-  return(
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3400);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const vid = videoRef.current;
+      if (vid) {
+        vid.play().catch(() => {});
+      }
+    }
+  }, []);
+
+  return loading ? (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <video
+        autoPlay
+        muted
+        playsInline
+        loop
+        className="max-w-4xl flex object-cover h-auto rounded-2xl"
+      >
+        <source src="/loading.mp4" type="video/mp4" />
+      </video>
+    </div>
+  ) : (
     <div className="bg-white min-h-screen helvetica">
-      <header className="bg-white py-6">
+      <header className="bg-white py-4">
         <div className="grid grid-cols-3 items-center">
           {/* left: brand */}
           <div className="flex items-center gap-3">
-            <a href="#" className="inline-flex items-baseline">
-              <Image src="/logo-black.png" width={180} height={180} alt="logo"/>
+            <a href="#" className="inline-flex items-baseline ml-4">
+              <Image
+                src="/logo-black.png"
+                width={180}
+                height={180}
+                alt="logo"
+              />
             </a>
           </div>
 
@@ -25,28 +65,36 @@ export default function Home() {
                   >
                     <p className="hover:scale-105 transition-transform">
                       {label}
-                      </p>
+                    </p>
                   </a>
                 </li>
               ))}
             </ul>
           </nav>
-              
+
           {/* right: tickets button */}
           <div className="flex flex-row justify-end">
             {/* tickets button */}
             <a
-              href="#"
-              className="ml-2 mr-8 inline-block bg-red-600 hover:bg-white hover:text-red-600 text-white text-sm font-semibold px-4 py-2 rounded-full shadow"
+              href="/registration"
+              className="ml-2 mr-8 inline-block uppercase hover:scale-105 transition-transform bg-red-600 hover:bg-red-700 text-white text-md font-bold px-4 py-4 rounded-2xl shadow "
             >
-              Registration
+              <ArrowTopRightOnSquareIcon className="inline-block w-5 h-5 mr-2 -mt-1" />
+              REGISTRATION
             </a>
           </div>
         </div>
       </header>
 
-      <main className="max-w-7xl mx-auto helvetica px-6">
-        <video autoPlay loop className="w-full h-auto rounded-2xl">
+      <main className="max-w-8xl pb-10 helvetica px-8">
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          playsInline
+          loop
+          className="w-full object-cover h-auto rounded-2xl"
+        >
           <source src="/looky.mp4" type="video/mp4" />
         </video>
       </main>
